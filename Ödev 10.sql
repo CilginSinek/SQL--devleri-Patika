1a8fbd2547d85e2e1071aeb59493d56f7812cd9e
Ismail Han <Cilginsinek58@gmail.com>
2022-07-17 23:01:23 +0300

    10. ödev

---
+++ Ödev 10.sql
@@ -0,0 +1,3 @@
+SELECT city AS Şehir, country AS Ülke FROM city LEFT JOIN country ON city.country_id = country.country_id;
+SELECT payment_id AS "Payment id", first_name AS "İsim", last_name AS "Soy isim" FROM payment RIGHT JOIN customer ON customer.customer_id = payment.customer_id;
+SELECT rental_id AS "Rental id", first_name AS "İsim", last_name AS "Soy isim" FROM customer FULL JOIN rental ON customer.customer_id = rental.customer_id;
